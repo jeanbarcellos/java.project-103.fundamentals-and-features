a7f70947ec43aacff087a1e4fbbd8fc3e10821e4
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-04-23 00:29:48 -0300

    Optional -  11. Transforming Value With map()

--- src/test/java/com/barcellos/OptionalTest.java
+++ src/test/java/com/barcellos/OptionalTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -304,4 +306,47 @@ public class OptionalTest {
         assertFalse(priceIsInRange2(null));
     }
 
+    // 11. Transforming Value With map()
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect() {
+        List<String> companyNames = Arrays.asList("paypal", "oracle", "", "microsoft", "", "apple");
+        Optional<List<String>> listOptional = Optional.of(companyNames);
+
+        int size = listOptional
+                .map(List::size)
+                .orElse(0);
+
+        assertEquals(6, size);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect2() {
+        String name = "baeldung";
+        Optional<String> nameOptional = Optional.of(name);
+
+        int len = nameOptional
+                .map(String::length)
+                .orElse(0);
+
+        assertEquals(8, len);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorksWithFilter_thenCorrect() {
+        String password = " password ";
+        Optional<String> passOpt = Optional.of(password);
+
+        boolean correctPassword = passOpt.filter(
+                pass -> pass.equals("password")).isPresent();
+
+        assertFalse(correctPassword);
+
+        correctPassword = passOpt
+                .map(String::trim)
+                .filter(pass -> pass.equals("password"))
+                .isPresent();
+
+        assertTrue(correctPassword);
+    }
 }
