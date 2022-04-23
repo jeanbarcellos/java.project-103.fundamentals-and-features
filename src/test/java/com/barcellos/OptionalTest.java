ab393f5d4703afaca3702231a665af6bb4c1082f
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-04-23 00:22:45 -0300

    Optional -  10. Conditional Return With filter()

--- src/test/java/com/barcellos/OptionalTest.java
+++ src/test/java/com/barcellos/OptionalTest.java
@@ -240,4 +240,68 @@ public class OptionalTest {
         String name = opt.get();
     }
 
+    // 10. Conditional Return With filter()
+
+    @Test
+    public void whenOptionalFilterWorks_thenCorrect() {
+        Integer year = 2016;
+        Optional<Integer> yearOptional = Optional.of(year);
+
+        boolean is2016 = yearOptional.filter(y -> y == 2016).isPresent();
+        assertTrue(is2016);
+
+        boolean is2017 = yearOptional.filter(y -> y == 2017).isPresent();
+        assertFalse(is2017);
+    }
+
+    public class Modem {
+        private Double price;
+
+        public Modem(Double price) {
+            this.price = price;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+    }
+
+    public boolean priceIsInRange1(Modem modem) {
+        boolean isInRange = false;
+
+        if (modem != null && modem.getPrice() != null
+                && (modem.getPrice() >= 10
+                        && modem.getPrice() <= 15)) {
+
+            isInRange = true;
+        }
+        return isInRange;
+    }
+
+    @Test
+    public void whenFiltersWithoutOptional_thenCorrect() {
+        assertTrue(priceIsInRange1(new Modem(10.0)));
+        assertFalse(priceIsInRange1(new Modem(9.9)));
+        assertFalse(priceIsInRange1(new Modem(null)));
+        assertFalse(priceIsInRange1(new Modem(15.5)));
+        assertFalse(priceIsInRange1(null));
+    }
+
+    public boolean priceIsInRange2(Modem modem2) {
+        return Optional.ofNullable(modem2)
+                .map(Modem::getPrice)
+                .filter(p -> p >= 10)
+                .filter(p -> p <= 15)
+                .isPresent();
+    }
+
+    @Test
+    public void whenFiltersWithOptional_thenCorrect() {
+        assertTrue(priceIsInRange2(new Modem(10.0)));
+        assertFalse(priceIsInRange2(new Modem(9.9)));
+        assertFalse(priceIsInRange2(new Modem(null)));
+        assertFalse(priceIsInRange2(new Modem(15.5)));
+        assertFalse(priceIsInRange2(null));
+    }
+
 }
