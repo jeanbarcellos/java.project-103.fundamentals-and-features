1d67e1ce8cbddf322936b8b9fd69527fbb3ae2c6
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-04-22 23:15:32 -0300

    Testes apenas

--- src/test/java/com/barcellos/AppTest.java
+++ src/test/java/com/barcellos/AppTest.java
@@ -1,5 +1,6 @@
 package com.barcellos;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,11 +9,20 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testSoma() {
+        int num1 = 1;
+        int num2 = 2;
+
+        assertEquals(3, num1 + num2);
     }
 }
