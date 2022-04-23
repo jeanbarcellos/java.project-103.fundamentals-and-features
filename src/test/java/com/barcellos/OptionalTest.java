335621b36302c26d5ec039023fcf763016f843d2
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-04-23 00:33:50 -0300

    Optional - 12. Transforming Value With flatMap()

--- src/test/java/com/barcellos/OptionalTest.java
+++ src/test/java/com/barcellos/OptionalTest.java
@@ -349,4 +349,47 @@ public class OptionalTest {
 
         assertTrue(correctPassword);
     }
+
+    // 12. Transforming Value With flatMap()
+
+    public class Person {
+        private String name;
+        private int age;
+        private String password;
+
+        public Optional<String> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        public Optional<Integer> getAge() {
+            return Optional.ofNullable(age);
+        }
+
+        public Optional<String> getPassword() {
+            return Optional.ofNullable(password);
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    @Test
+    public void givenOptional_whenFlatMapWorks_thenCorrect2() {
+        Person person = new Person("john", 26);
+        Optional<Person> personOptional = Optional.of(person);
+
+        Optional<Optional<String>> nameOptionalWrapper = personOptional.map(Person::getName);
+        Optional<String> nameOptional = nameOptionalWrapper.orElseThrow(IllegalArgumentException::new);
+        String name1 = nameOptional.orElse("");
+        assertEquals("john", name1);
+
+        String name = personOptional
+                .flatMap(Person::getName)
+                .orElse("");
+
+        assertEquals("john", name);
+    }
+
 }
