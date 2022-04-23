a32b58e8ba52ce5e3f37dfa50071303021da5918
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-04-23 17:26:04 -0300

    Optional - Java 9 - 3. O método ifPresentOrElse()

--- src/test/java/com/barcellos/optional/OptionalJava9Test.java
+++ src/test/java/com/barcellos/optional/OptionalJava9Test.java
@@ -3,6 +3,7 @@ package com.barcellos.optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 
@@ -24,6 +25,7 @@ public class OptionalJava9Test {
      */
     @Test
     public void givenOptional_whenPresent_thenShouldTakeAValueFromIt() {
+
         // given
         String expected = "properValue";
         Optional<String> value = Optional.of(expected);
@@ -42,6 +44,7 @@ public class OptionalJava9Test {
      */
     @Test
     public void givenOptional_whenEmpty_thenShouldTakeAValueFromOr() {
+
         // given
         String defaultString = "default";
         Optional<String> value = Optional.empty();
@@ -52,5 +55,56 @@ public class OptionalJava9Test {
 
         // then
         assertThat(result.get()).isEqualTo(defaultString);
+    }
+
+    // 3. O método ifPresentOrElse()
+
+    /**
+     * Quando temos uma instância Optional, geralmente queremos executar uma ação
+     * específica no valor subjacente dela. Por outro lado, se o Optional estiver
+     * vazio, queremos registrá-lo ou rastrear esse fato incrementando alguma
+     * métrica.
+     *
+     * O método ifPresentOrElse() é criado exatamente para esses cenários. Podemos
+     * passar um Consumer que será invocado se o Optional estiver definido, e
+     * Runnable que será executado se o Optional estiver vazio.
+     *
+     * Digamos que temos um Optional definido e queremos incrementar um contador
+     * específico se o valor estiver presente:
+     */
+    @Test
+    public void givenOptional_whenPresent_thenShouldExecuteProperCallback() {
+
+        // given
+        Optional<String> value = Optional.of("properValue");
+        AtomicInteger successCounter = new AtomicInteger(0);
+        AtomicInteger onEmptyOptionalCounter = new AtomicInteger(0);
+
+        // when
+        value.ifPresentOrElse(
+                v -> successCounter.incrementAndGet(),
+                onEmptyOptionalCounter::incrementAndGet);
+
+        // then
+        assertThat(successCounter.get()).isEqualTo(1);
+        assertThat(onEmptyOptionalCounter.get()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenOptional_whenNotPresent_thenShouldExecuteProperCallback() {
+
+        // given
+        Optional<String> value = Optional.empty();
+        AtomicInteger successCounter = new AtomicInteger(0);
+        AtomicInteger onEmptyOptionalCounter = new AtomicInteger(0);
+
+        // when
+        value.ifPresentOrElse(
+                v -> successCounter.incrementAndGet(),
+                onEmptyOptionalCounter::incrementAndGet);
+
+        // then
+        assertThat(successCounter.get()).isEqualTo(0);
+        assertThat(onEmptyOptionalCounter.get()).isEqualTo(1);
     }
 }
